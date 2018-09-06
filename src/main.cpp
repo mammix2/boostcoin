3ba785e4f3c4c87db8919c7ea6b81619743636d9
mammix2 <mammix2@hotmail.com>
2018-09-06 09:49:29 +0100

    coinage calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1759,10 +1759,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
         LogPrint("[DEBUG] - nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN: nSubsidy=%d \n", nSubsidy);
     }
 
-    nSubsidy = nCoinAge * nRewardCoinYear / 365;
-    LogPrint("[DEBUG] - return nSubsidy + nFees; =%d \n", (nSubsidy + nFees));
     return nSubsidy + nFees;
-
 }
 
 bool IsInitialBlockDownload()
