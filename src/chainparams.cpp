600939e8d7995d4d35933992ee8fced55e435ba9
mammix2 <mammix2@hotmail.com>
2018-09-11 00:37:02 +0100

    generate - max iterations

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -204,8 +204,8 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1533081600; // August 1st, 2018
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1564617600; // August 1st, 2019
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1543622400; // Saturday, 1 December 2018 00:00:00 GMT+00:00
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1575158400; // Sunday, 1 December 2019 00:00:00 GMT+00:00
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000100010");
