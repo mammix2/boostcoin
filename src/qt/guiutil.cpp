1d71a79f80d90e1d0ef1b4c4a42ddb0d52c4e3fc
mammix2 <mammix2@hotmail.com>
2018-10-25 08:46:38 +0100

    naming changes

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -710,8 +710,8 @@ boost::filesystem::path static GetAutostartFilePath()
 {
     std::string chain = ChainNameFromCommandLine();
     if (chain == CBaseChainParams::MAIN)
-        return GetAutostartDir() / "bitcoin.desktop";
-    return GetAutostartDir() / strprintf("bitcoin-%s.lnk", chain);
+        return GetAutostartDir() / "boostcoin.desktop";
+    return GetAutostartDir() / strprintf("boostcoin-%s.lnk", chain);
 }
 
 bool GetStartOnSystemStartup()
