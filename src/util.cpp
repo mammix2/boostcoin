473e6335de9350da7e0b6b6c9962bfa61726b18f
mammix2 <mammix2@hotmail.com>
2021-04-20 22:14:30 +0100

    boostcoin data directory

--- src/util.cpp
+++ src/util.cpp
@@ -455,13 +455,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Boostcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Boostcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\boostcoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\boostcoin
+    // Mac: ~/Library/Application Support/boostcoin
+    // Unix: ~/.boostcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Boostcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "boostcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -471,7 +471,7 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Boostcoin";
+    return pathRet / "Library/Application Support/boostcoin";
 #else
     // Unix
     return pathRet / ".boostcoin";
