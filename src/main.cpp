cce3c9ca893d55a9b84da60fac9b4386473c4592
mammix2 <mammix2@hotmail.com>
2021-04-21 15:36:02 +0100

    Travis-CI

--- src/main.cpp
+++ src/main.cpp
@@ -1742,14 +1742,14 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
 
     int64_t nRewardCoinYear;
-//	int64_t nSubsidy;
+	int64_t nSubsidy;
     nRewardCoinYear = MAX_MINT_PROOF_OF_STAKE;
 	
-//    if (pindexBestHeader->nHeight < 641250 ) {
-    int64_t nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
-//		} else {
-//			nSubsidy = nCoinAge * nRewardCoinYear / 365;
-//		}
+    if (pindexBestHeader->nHeight < 641250 ) {
+    	nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
+	} else {
+		nSubsidy = nCoinAge * nRewardCoinYear / 365;
+	}
 
     return nSubsidy + nFees;
 }
