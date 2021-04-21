4e0f94f087e6c9629ef87c50419a5e1521e26f0f
mammix2 <mammix2@hotmail.com>
2021-04-21 09:20:51 +0100

    names

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -184,7 +184,7 @@ int main(int argc, char* argv[])
 {
     SetupEnvironment();
 
-    // Connect bitcoind signal handlers
+    // Connect boostcoind signal handlers
     noui_connect();
 
     return (AppInit(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE);
